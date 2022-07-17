5387a22f3d1c60e5efd003516c13061a17e0f3bc
Martin Diaz <cavallinorampante@gmail.com>
2022-07-17 18:03:51 -0300

    se agrego el backup de la base de datos del proyeto arcchivo cacproyecto2022

---
+++ cacproyecto2022.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-07-2022 a las 22:45:45
+-- Versión del servidor: 8.0.17
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cacproyecto2022`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localidades`
+--
+
+CREATE TABLE `localidades` (
+  `idlocalidad` int(11) NOT NULL,
+  `nombrelocalidad` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `idprovincialocalidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `localidades`
+--
+
+INSERT INTO `localidades` (`idlocalidad`, `nombrelocalidad`, `idprovincialocalidad`) VALUES
+(1, 'Moreno', 3),
+(2, 'Guaymallen', 4),
+(3, 'Cayafate', 2),
+(4, 'Cruz Alta', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `idpedido` int(11) NOT NULL,
+  `nombre` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `apellido` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `usuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `lugarentrega` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
+  `localidad` int(11) NOT NULL,
+  `provincia` int(11) NOT NULL,
+  `codigopostal` varchar(9) COLLATE utf8_spanish_ci NOT NULL,
+  `formadepago` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
+  `tarjtitular` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `tarjnumero` varchar(16) COLLATE utf8_spanish_ci NOT NULL,
+  `tarjvto` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `tarjclave` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincias`
+--
+
+CREATE TABLE `provincias` (
+  `idprovincia` int(11) NOT NULL,
+  `nombre` varchar(40) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `provincias`
+--
+
+INSERT INTO `provincias` (`idprovincia`, `nombre`) VALUES
+(1, 'Tucuman'),
+(2, 'Salta'),
+(3, 'Buenos Aires'),
+(4, 'Mendoza');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idusuario` int(11) NOT NULL,
+  `usuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `clave` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `nombreyapellido` varchar(40) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idusuario`, `usuario`, `clave`, `nombreyapellido`) VALUES
+(1, 'prueba@gmail.com', '909090', 'Mircea Eliade'),
+(2, 'fulano@gmail.com', '919191', 'Cosme Fulanito');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `localidades`
+--
+ALTER TABLE `localidades`
+  ADD PRIMARY KEY (`idlocalidad`),
+  ADD KEY `idprovincialocalidad` (`idprovincialocalidad`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`idpedido`);
+
+--
+-- Indices de la tabla `provincias`
+--
+ALTER TABLE `provincias`
+  ADD PRIMARY KEY (`idprovincia`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `localidades`
+--
+ALTER TABLE `localidades`
+  MODIFY `idlocalidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `idpedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `provincias`
+--
+ALTER TABLE `provincias`
+  MODIFY `idprovincia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `localidades`
+--
+ALTER TABLE `localidades`
+  ADD CONSTRAINT `localidades_ibfk_1` FOREIGN KEY (`idprovincialocalidad`) REFERENCES `provincias` (`idprovincia`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
